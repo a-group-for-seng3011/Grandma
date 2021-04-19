9fe9ae299b4d5074491f7bccacfaf8e3d213b75e
William Chan <william.chan4@gmail.com>
2021-04-19 15:34:44 +1000

    Outbreak API service and test script. Run "node test.js" in command line to test

---
+++ test.js
@@ -0,0 +1,11 @@
+const OutbreakAPI = require("./services/outbreak-api");
+
+
+async function myfunc() {
+    const outbreakAPI = new OutbreakAPI(undefined, undefined, 'Coronavirus', undefined);
+    const articles = await outbreakAPI.getSortedArticles();
+
+    console.log(articles);
+}
+
+myfunc();
